4b039c83c18431f86f169c5081d976d5c4955fcb
suhrob <suxrobamirov00@gmail.com>
2025-01-31 11:02:17 +0500

    ada

--- 4-dars/home/carusel/js/app.js
+++ 4-dars/home/carusel/js/app.js
@@ -1,27 +1,27 @@
 let index = 0;
 const carousel = document.getElementById("carusel1");
-// const a1 = document.getElementById("a1");
-// const a2 = document.getElementById("a2");
-// const a3 = document.getElementById("a3");
-// const c1 = document.getElementById("c1");
-// const c2 = document.getElementById("c2");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const c1 = document.getElementById("c1");
+const c2 = document.getElementById("c2");
 
-// a1.addEventListener("click", ()=>{
-//     c1.style.display = "block";
-//     a1.style.backgroundColor = "blue"
-//     a2.style.backgroundColor = "black"
-//     a3.style.backgroundColor = "black"
-//     c2.style.display = "none";
-//     // c3.style.display = "none";
-// });
-// a2.addEventListener("click", ()=>{
-//     c2.style.display = "block";
-//     a2.style.backgroundColor = "blue"
-//     a1.style.backgroundColor = "black"
-//     a3.style.backgroundColor = "black"
-//     c1.style.display = "none";
-//     // c3.style.display = "none";
-// })
+a1.addEventListener("click", ()=>{
+    c1.style.display = "block";
+    a1.style.backgroundColor = "blue"
+    a2.style.backgroundColor = "black"
+    a3.style.backgroundColor = "black"
+    c2.style.display = "none";
+    // c3.style.display = "none";
+});
+a2.addEventListener("click", ()=>{
+    c2.style.display = "block";
+    a2.style.backgroundColor = "blue"
+    a1.style.backgroundColor = "black"
+    a3.style.backgroundColor = "black"
+    c1.style.display = "none";
+    // c3.style.display = "none";
+})
 
 
 
@@ -41,20 +41,20 @@ setInterval(nextSlide, 3000);
 
 
 
-// let index2 = 0;
-// const carousel2 = document.getElementById("carusel2");
+let index2 = 0;
+const carousel2 = document.getElementById("carusel2");
 
-// function showSlide2() {
-//     carousel2.style.transform = `translateX(${-index2 * 700}px)`;
-// }
-// function nextSlide2() {
-//     index2 = (index2 + 1) % 5;
-//     showSlide2();
-// }
-// function prevSlide2() {
-//     index2 = (index2 - 1 + 5) % 5;
-//     showSlide2();
-// }
+function showSlide2() {
+    carousel2.style.transform = `translateX(${-index2 * 700}px)`;
+}
+function nextSlide2() {
+    index2 = (index2 + 1) % 5;
+    showSlide2();
+}
+function prevSlide2() {
+    index2 = (index2 - 1 + 5) % 5;
+    showSlide2();
+}
 
 
 
